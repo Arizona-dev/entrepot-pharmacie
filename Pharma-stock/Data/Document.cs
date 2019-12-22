d286f62a446d9f15e65256bbed79d31e434bfd0d
Naderi Farid <geekwid@gmail.com>
2019-12-22 20:51:34 +0100

    ajout word

---
+++ Pharma-stock/Data/Document.cs
@@ -0,0 +1,39 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    class Document
+    {
+        private Body body;
+
+        public Document()
+        {
+        }
+
+        public void CreerFacture()
+        {
+            using (WordprocessingDocument doc = WordprocessingDocument.Create("C:\\test11.docx", DocumentFormat.OpenXml.WordprocessingDocumentType.Document))
+            {
+                // Add a main document part.
+                MainDocumentPart mainPart = doc.AddMainDocumentPart();
+
+                // Create the document structure and add some text.
+                mainPart.Document = new Document();
+                Body body = mainPart.Document.AppendChild(new Body());
+                Paragraph para = body.AppendChild(new Paragraph());
+                Run run = para.AppendChild(new Run());
+
+                // String msg contains the text, "Hello, Word!"
+                run.AppendChild(new Text("New text in document"));
+            }
+        }
+
+    }
+}
